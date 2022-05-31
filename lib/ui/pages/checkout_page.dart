8f1f6a3bbcc4df08958b2aefd59cd7655f78cfae
Rian Wahyu <rianwahyu26@gmail.com>
2022-05-31 15:46:26 +0700

    Success CheckOut

--- lib/ui/pages/checkout_page.dart
+++ lib/ui/pages/checkout_page.dart
@@ -1,4 +1,5 @@
 import 'package:airplane/shared/theme.dart';
+import 'package:airplane/ui/pages/success_checkout_page.dart';
 import 'package:airplane/ui/widgets/booking_details_item.dart';
 import 'package:airplane/ui/widgets/custom_button.dart';
 import 'package:flutter/material.dart';
@@ -314,7 +315,14 @@ class CheckOutPage extends StatelessWidget {
     Widget payNowButton() {
       return CustomButton(
         title: 'Pay Now',
-        onPressed: () {},
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => SuccessCheckoutPage(),
+            ),
+          );
+        },
         margin: EdgeInsets.only(
           top: 16,
         ),
