bdd48dd177005f3be40a14e89f23a189de32c58d
Rian Wahyu <rianwahyu26@gmail.com>
2022-06-02 16:56:41 +0700

    menambahkan method fetch destination

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:airplane/cubit/auth_cubit.dart';
+import 'package:airplane/cubit/destination_cubit.dart';
 import 'package:airplane/cubit/page_cubit.dart';
 import 'package:airplane/ui/pages/bonus_page.dart';
 import 'package:airplane/ui/pages/get_started_page.dart';
@@ -28,6 +29,7 @@ class MyApp extends StatelessWidget {
           create: (context) => PageCubit(),
         ),
         BlocProvider(create: (context) => AuthCubit()),
+        BlocProvider(create: (context) => DestinationCubit()),
       ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
