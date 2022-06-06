5a5ce5f77f54ba4903da29aebe1fdd35752e36a4
Rian Wahyu <rianwahyu26@gmail.com>
2022-06-06 16:53:56 +0700

    menampilkan hasil transaksi

--- lib/cubit/transaction_state.dart
+++ lib/cubit/transaction_state.dart
@@ -11,7 +11,15 @@ class TransactionInitial extends TransactionState {}
 
 class TransactionLoading extends TransactionState {}
 
-class TransactionSuccess extends TransactionState {}
+class TransactionSuccess extends TransactionState {
+  final List<TransactionModel> transactions;
+
+  TransactionSuccess(this.transactions);
+
+  @override
+  // TODO: implement props
+  List<Object> get props => [transactions];
+}
 
 class TransactionFailed extends TransactionState {
   final String error;
